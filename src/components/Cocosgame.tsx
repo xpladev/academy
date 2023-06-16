1beed3bb85ae982cd0a7d1dadb8cb4d0c8633e96
nglsena0722c <nglsena0722@com2us.com>
2023-06-16 16:10:37 +0900

    Fix : Fix size of game

--- src/components/Cocosgame.tsx
+++ src/components/Cocosgame.tsx
@@ -4,7 +4,7 @@ export default function Cocosgame() {
   const iframe = '<iframe src="/xpla-academy-dev/img/web-desktop/index.html" width="100%" height="100%"></iframe>';
   return (
     <div
-      style={{ width: 1440, height: 1200 }}
+      style={{ width: 900, height: 780 }}
       dangerouslySetInnerHTML={{ __html: iframe }}
     ></div>
   );
