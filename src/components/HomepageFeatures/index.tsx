93ff97edc4d42bae771a45e3af1480bbb6db9773
nglsena0722c <nglsena0722@com2us.com>
2023-06-30 10:31:01 +0900

    Fix : Fix Homepage Design

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,37 +4,37 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: string;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Easy to follow',
+    Svg: '/xpla-academy-dev/img/homepage/follow.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
+        따라하기 쉬운 난이도 Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Freely moddable',
+    Svg: '/xpla-academy-dev/img/homepage/free.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        코드 이용의 높은 자유도 Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
         ahead and move your docs into the <code>docs</code> directory.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Game developer-friendly',
+    Svg: '/xpla-academy-dev/img/homepage/friend.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
+        게임 개발자 친화적 Extend or customize your website layout by reusing React. Docusaurus can
         be extended while reusing the same header and footer.
       </>
     ),
@@ -45,7 +45,7 @@ function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={Svg} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
