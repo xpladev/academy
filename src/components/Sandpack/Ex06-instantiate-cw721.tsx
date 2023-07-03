e338fa391ae4a0ea11e47fb706a9955dd34e8f85
nglsena0722c <nglsena0722@com2us.com>
2023-07-03 18:19:36 +0900

    Feat : Write 2-make-nft

---
+++ src/components/Sandpack/Ex06-instantiate-cw721.tsx
@@ -0,0 +1,63 @@
+import {
+    Sandpack,
+  } from "@codesandbox/sandpack-react";
+  import React from "react";
+  
+  const Ex06 = () => {
+    return (
+      <Sandpack
+        customSetup={{
+          dependencies: {
+            "@xpla/xpla.js": "^0.3.1",
+            "crypto-browserify" : "latest"
+          },
+          entry: "index.js",
+        }}
+        files={{
+          "/index.js": `const { LCDClient, MnemonicKey, MsgInstantiateContract } = require("@xpla/xpla.js");
+
+const lcd = new LCDClient({
+  chainID: 'cube_47-5',
+  URL: 'https://cube-lcd.xpla.dev'
+});
+
+const main = async () => {
+  const mk = new MnemonicKey({
+    mnemonic: 'myth snow ski simple century dad gun dolphin sail lawsuit fringe image toast betray frown keep harbor flash table prevent isolate panic tag vehicle' // 여러분의 니모닉 단어로 바꿔주세요.
+  })
+
+  const wallet = lcd.wallet(mk);
+  const myWalletAddress = wallet.key.accAddress;
+
+  const init_msg = {
+    "name": "YourNFTName",
+    "symbol": "YNN",
+    "minter": myWalletAddress
+  }
+
+  const instantiate = new MsgInstantiateContract(
+    myWalletAddress, // sender
+    myWalletAddress, // admin
+    3, // MAINNET, TESTNET CW721 code id
+    init_msg,
+    {}, // 컨트랙트에 전송할 XPLA 토큰 수량, 현재는 필요하지 않으니 비워둡니다.
+    'Input your label', // 작성하고 싶은 라벨을 입력해주세요.
+  );
+
+  const signedTx = await lcd.wallet(mk).createAndSignTx({ // 트랜잭션 생성 및 사이닝, 트랜잭션 fee 설정
+    msgs: [instantiate]
+  });
+
+  const txResult = await lcd.tx.broadcastSync(signedTx);
+  console.log(txResult.txhash);
+}
+main()
+          `,
+        }}
+        options={{ layout: "console" }}
+      />
+    );
+  };
+  
+  export default Ex06;
+  
---
+++ src/components/Sandpack/Ex07-mint-nft.tsx
@@ -0,0 +1,82 @@
+import {
+    Sandpack,
+  } from "@codesandbox/sandpack-react";
+  import React from "react";
+  
+  const Ex07 = () => {
+    return (
+      <Sandpack
+        customSetup={{
+          dependencies: {
+            "@xpla/xpla.js": "^0.3.1",
+            "crypto-browserify" : "latest"
+          },
+          entry: "index.js",
+        }}
+        files={{
+          "/index.js": `const { LCDClient, MnemonicKey, MsgExecuteContract } = require("@xpla/xpla.js");
+
+const lcd = new LCDClient({
+  chainID: 'cube_47-5',
+  URL: 'https://cube-lcd.xpla.dev'
+});
+
+const main = async () => {
+  const mk = new MnemonicKey({
+    mnemonic: 'myth snow ski simple century dad gun dolphin sail lawsuit fringe image toast betray frown keep harbor flash table prevent isolate panic tag vehicle' // 여러분의 니모닉 단어로 바꿔주세요.
+  })
+
+  const wallet = lcd.wallet(mk);
+  const myWalletAddress = wallet.key.accAddress;
+  const myContractAddress = "xpla10r58td65tgvpaqngg7802rtpg5l6as8my2unnlnpa8en894qqstshxq0u0"; // Input your Contract Address
+
+  const time = new Date();
+  const mintJSON = {
+    mint: {
+      owner: myWalletAddress,
+      token_id: \`token_id_\${time.getTime()}\`,
+      // token_uri: "https://your.token.uri.address", // input your token_uri
+      extension: {
+        name : "My Test XPLA NFT",
+        // image: "https://your.image.url", // input your nft image url
+        attributes: [
+          {
+            value: 'Alice',
+            trait_type: 'CharacterName',
+          },
+          {
+            value: '100',
+            trait_type: 'CP',
+          },
+          {
+            value: '10',
+            trait_type: 'HP',
+          },
+        ],
+        description : "This is my First NFT in XPLA Blockchain!",
+      },
+    },
+  };
+  const mint = new MsgExecuteContract(
+    myWalletAddress,
+    myContractAddress,
+    mintJSON
+  );
+
+  const signedTx = await lcd.wallet(mk).createAndSignTx({ // 트랜잭션 생성 및 사이닝, 트랜잭션 fee 설정
+    msgs: [mint]
+  });
+
+  const txResult = await lcd.tx.broadcastSync(signedTx);
+  console.log(txResult.txhash);
+}
+main()
+          `,
+        }}
+        options={{ layout: "console" }}
+      />
+    );
+  };
+  
+  export default Ex07;
+  
