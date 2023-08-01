fcb520ea852ef494637d86319629a1f9dd4dce62
nglsena0722c <nglsena0722@com2us.com>
2023-08-01 12:54:27 +0900

    Fix : Fix docs file name

--- sidebars.js
+++ sidebars.js
@@ -86,7 +86,7 @@ const sidebars = {
         title: '게임에 Web 3.0 기능 더하기 2',
         // description: ''
       },
-      items: ['tutorial/add-func-to-game-2/overview', 'tutorial/add-func-to-game-2/account-sequence', 'tutorial/add-func-to-game-2/walletprovider', 'tutorial/add-func-to-game-2/convert', 'tutorial/add-func-to-game-2/db-contract'],
+      items: ['tutorial/add-func-to-game-2/overview', 'tutorial/add-func-to-game-2/account-sequence', 'tutorial/add-func-to-game-2/walletprovider', 'tutorial/add-func-to-game-2/convert', 'tutorial/add-func-to-game-2/write-contract'],
     },
   ],
 };
