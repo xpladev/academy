327270a5ea9c5099c49900e6e54fceb8f9c3c5ff
nglsena0722c <nglsena0722@com2us.com>
2023-08-09 13:40:11 +0900

    Fix : Fix name and structure of files

--- sidebars.js
+++ sidebars.js
@@ -27,10 +27,10 @@ const sidebars = {
     },
     {
       type: 'category',
-      label: 'Create Wallet',
+      label: '지갑 생성하기',
       link: {
         type: 'generated-index',
-        title: 'Create Wallet',
+        title: '지갑 생성하기',
         // description: '블록체인에서 토큰을 보관하고, 전송하려면 무엇이 필요할까요? 바로 지갑입니다. 직접 지갑을 만들어 보면서 자세히 살펴봅시다.'
         description: '일상에서 현금을 보관하기 위해 지갑을 사용하는 것처럼, 블록체인에서도 토큰을 보관하기 위해 지갑이 필요합니다. 직접 지갑을 만들어 보면서 자세히 살펴봅시다.'
       },
@@ -38,10 +38,10 @@ const sidebars = {
     },
     {
       type: 'category',
-      label: 'Create Testnet Transaction',
+      label: '트랜잭션 생성하기',
       link: {
         type: 'generated-index',
-        title: 'Create Testnet Transaction',
+        title: '트랜잭션 생성하기',
         description: 'XPLA 테스트넷 토큰을 이용하여 블록체인에 데이터를 기록해봅시다.'
       },
       items: ['settings/create-testnet-transaction/send-tx', 'settings/create-testnet-transaction/check-tx-in-explorer', 'settings/create-testnet-transaction/send-query', 'settings/create-testnet-transaction/use-xplad'],
@@ -66,7 +66,7 @@ const sidebars = {
         title: '게임 토큰(CW20) 다루기',
         // description: ''
       },
-      items: ['tutorial/make-web3-game/download-code', 'tutorial/make-web3-game/make-cw20-with-vault', 'tutorial/make-web3-game/make-cw20-with-js', 'tutorial/make-web3-game/query-cw20'],
+      items: ['tutorial/make-cw20/make-cw20-with-vault', 'tutorial/make-cw20/make-cw20-with-js', 'tutorial/make-cw20/query-cw20'],
     },
     {
       type: 'category',
@@ -76,7 +76,7 @@ const sidebars = {
         title: 'NFT(CW721) 다루기',
         // description: ''
       },
-      items: ['tutorial/add-func-to-game-1/overview', 'tutorial/add-func-to-game-1/make-nft-vault', 'tutorial/add-func-to-game-1/make-nft-js', 'tutorial/add-func-to-game-1/query-cw721'],
+      items: ['tutorial/make-cw721/make-nft-vault', 'tutorial/make-cw721/make-nft-js', 'tutorial/make-cw721/query-cw721'],
     },
     {
       type: 'category',
@@ -86,7 +86,7 @@ const sidebars = {
         title: '심화 - XPLA 블록체인 깊게 이해하기',
         // description: ''
       },
-      items: ['tutorial/add-func-to-game-2/local-network', 'tutorial/add-func-to-game-2/account-sequence', 'tutorial/add-func-to-game-2/walletprovider', 'tutorial/add-func-to-game-2/write-contract'],
+      items: ['tutorial/deep-understand-xpla/local-network', 'tutorial/deep-understand-xpla/account-sequence', 'tutorial/deep-understand-xpla/walletprovider', 'tutorial/deep-understand-xpla/write-contract'],
     },
   ],
 };
