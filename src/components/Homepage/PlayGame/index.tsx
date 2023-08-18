19de0ea2235040db580ac40b70389e40159c7985
nglsena0722c <nglsena0722@com2us.com>
2023-08-18 21:14:29 +0900

    Fix : Fix playgame in Mobile

--- src/components/Homepage/PlayGame/index.tsx
+++ src/components/Homepage/PlayGame/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import Cocosgame from "../../Cocosgame";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function PlayGame(): JSX.Element {
+  const matches = useMediaQuery('(max-width:768px)');
+
   return (
-    <section className="h-[1272px] flex flex-col justify-center items-center gap-[78px] px-4">
+    <section className="h-[900px] md:h-[1272px] flex flex-col justify-center items-center gap-[78px] px-4">
       <div className="flex flex-col items-center justify-center">
         <div className="font-bold text-[50px] leading-[60px]">
           Explore & Play the demo
         </div>
+        {
+          !matches &&
         <div className="font-bold text-[50px] leading-[60px]">
           Experience game building on XPLA
         </div>
+        }
       </div>
       <img src="/img/PlayGame/comingsoon.svg" />
       {/* <Cocosgame /> */}
