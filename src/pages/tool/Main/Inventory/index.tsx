94bfb0dd6bae018c5e1e424434bc037a3b90fcd3
nglsena0722c <nglsena0722@com2us.com>
2023-11-07 17:21:51 +0900

    Feat : Add Empty NFT Image

--- src/pages/tool/Main/Inventory/index.tsx
+++ src/pages/tool/Main/Inventory/index.tsx
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from "react";
 import BigNumber from "bignumber.js";
-import { useWallet } from "@xpla/wallet-provider";
-import axios from "axios";
 import "./Inventory.css";
 import clsx from "clsx";
 import NFTInfo from "./NFTInfo";
-import useUserAddress from "@site/src/hooks/Zustand/useUserAddress";
 import useUserInfo from "@site/src/hooks/useQuery/useUserInfo";
 import useWalletNftList from "../../../../hooks/useQuery/useWalletNftList";
+import _ from "lodash";
 
 export default function Inventory() {
-  const { userAddress } = useUserAddress();
   const { data: userInfo } = useUserInfo();
 
   const [page, setPage] = useState<number>(1);
   const [maxPage, setMaxPage] = useState<number>(1);
   const numPerPage = 8;
 
-  const {data : nftlistRes, status } = useWalletNftList();
+  const { data: nftlistRes } = useWalletNftList();
+  const [userNft, setUserNft] = useState<string[]>();
 
   const bigXPLABalance = new BigNumber(userInfo?.xpla || "0")
     .toFormat(18, {
@@ -39,7 +37,11 @@ export default function Inventory() {
   });
 
   useEffect(() => {
-    if (nftlistRes && nftlistRes.returnMsg === "success" && nftlistRes.tokenList.tokens.length > 0) {
+    if (
+      nftlistRes &&
+      nftlistRes.returnMsg === "success" &&
+      nftlistRes.tokenList.tokens.length > 0
+    ) {
       const len = nftlistRes.tokenList.tokens.length;
       if (len % numPerPage === 0) {
         setMaxPage(Math.floor(len / numPerPage));
@@ -49,6 +51,17 @@ export default function Inventory() {
     }
   }, [nftlistRes]);
 
+  useEffect(() => {
+    if (nftlistRes && nftlistRes.returnMsg === "success") {
+      setUserNft(
+        nftlistRes.tokenList.tokens.slice(
+          (page - 1) * numPerPage,
+          page * numPerPage
+        )
+      );
+    }
+  }, [nftlistRes, page]);
+
   return (
     <div className="max-w-[380px] bg-[#EAF8FF] flex flex-col items-center justify-between p-[20px]">
       <span className="mt-[5px] font-semibold text-[28px] leading-[33px]">
@@ -57,17 +70,22 @@ export default function Inventory() {
       <div
         className={clsx(
           "grid  gap-[8px] w-full mt-[16px] min-h-[165px]",
-          nftlistRes?.tokenList.tokens.length === 0 ? "grid-cols-1" : "grid-cols-4"
+          nftlistRes?.tokenList.tokens.length === 0
+            ? "grid-cols-1"
+            : "grid-cols-4"
         )}
       >
-        {/* TODO : 8개만 slice해서 가져오고, 8개보다 적게 있으면 빈칸으로 추가해주기 */}
-        {nftlistRes &&
-          nftlistRes?.tokenList.tokens
-            .slice((page - 1) * numPerPage, page * numPerPage)
-            .map((userNFT) => (
-              <NFTInfo tokenId={userNFT} key={userNFT} />
-            ))}
-        {nftlistRes?.tokenList.tokens.length === 0 && <span>There is no nftlist item.</span>}
+        {userNft?.map((tokenId) => (
+          <NFTInfo tokenId={tokenId} key={tokenId} />
+        ))}
+        {_.range(numPerPage - (userNft?.length || 0)).map((idx) => (
+          <div
+            key={idx + "emptyInventory"}
+            className="border-[1px] border-solid aspect-square bg-white flex justify-center items-center"
+          >
+            <img src="/img/tool/Main/Info/emptyImg.svg" />
+          </div>
+        ))}
       </div>
 
       <div className="flex items-center gap-[15px] py-[26px]">
