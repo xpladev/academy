4dd7b9b2d8254e006c97fd892341bf9dcbf672b9
nglsena0722c <nglsena0722@com2us.com>
2024-03-14 14:48:44 +0900

    Fix : Fix mobile height of GoogleForm

--- src/pages/submit/index.tsx
+++ src/pages/submit/index.tsx
@@ -9,7 +9,7 @@ export default function Submit(): JSX.Element {
     >
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSfYJ7cizoWyPFv9ISw4S2uyIutn_WaEIVX1YQA6nRrJrRi0Eg/viewform?embedded=true"
-        className="w-full h-[1200px] my-[20px] overflow-hidden"
+        className="w-full h-[1300px] md:h-[1200px] my-[20px] overflow-hidden"
         frameborder="0"
         marginheight="0"
         marginwidth="0"
